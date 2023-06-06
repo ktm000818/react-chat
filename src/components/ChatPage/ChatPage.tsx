a9b3e49139d2242fb11fa197d0d72ff5284524ec
ktm000818 <kimtm0818@gmail.com>
2023-06-06 23:52:27 +0900

    Feat: 채팅 페이지 로그인 체크 적용

--- src/components/ChatPage/ChatPage.tsx
+++ src/components/ChatPage/ChatPage.tsx
@@ -1,7 +1,9 @@
+import useLoginCheck from "@/custom-hooks/useLoginCheck";
 import MainPanel from "./MainPanel/MainPanel";
 import SidePanel from "./SidePanel/SidePanel";
 
 export default function ChatPage() {
+  useLoginCheck();
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: "500px" }}>
