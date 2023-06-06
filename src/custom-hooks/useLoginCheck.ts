b4c156594e8c511c0a8987a4e2b8677e7c3637b9
ktm000818 <kimtm0818@gmail.com>
2023-06-06 23:52:13 +0900

    Feat: 로그인 체크 로직 변경

--- src/custom-hooks/useLoginCheck.ts
+++ src/custom-hooks/useLoginCheck.ts
@@ -3,16 +3,24 @@ import { useNavigate } from "react-router-dom";
 
 export default function useLoginCheck(){
     const navigate = useNavigate();
+    const ACCESS_PAGES = ['/chat', '/'];
+    const ACCESS_FOR_UNKNOWN_PAGES = ['/login', '/register', '/'];
 
     useEffect(() => {
         const recoilPersist = localStorage.getItem("recoil-persist")
+        const currLocation = window.location.pathname;
+        
         if(recoilPersist){
             if(JSON.parse(recoilPersist)?.loginState?.uid){
-                if(window.location.pathname !== "/"){
+                //로그인 완료됐을 때
+                if(!ACCESS_PAGES.includes(currLocation)){
                     navigate("/");
                 }
             }else{
-                // navigate("/");
+                //로그인 되지않았을 때
+                if(!ACCESS_FOR_UNKNOWN_PAGES.includes(currLocation)){
+                    navigate("/");
+                }
             }
         }
     }, [])
