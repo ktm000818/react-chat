57e7dd12ea6bfe8f544a949bf0f818fea73e670b
ktm000818 <kimtm0818@gmail.com>
2023-06-07 17:38:55 +0900

    chore: recoil selector 정의

--- src/recoil/recoil-store/store.js
+++ src/recoil/recoil-store/store.js
@@ -1,9 +1,29 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist();
 
-export const loginState = atom({
-  key: "loginState",
+export const loginUserState = atom({
+  key: "loginUserState",
   default: null,
   effects: [persistAtom],
+});
+
+export const userNameSelector = selector({
+  key: "userName",
+  get: ({ get }) => {
+    const user = get(loginUserState);
+    if (user) {
+      return user.displayName;
+    }
+  },
+});
+
+export const userImageSelector = selector({
+  key: "userImage",
+  get: ({ get }) => {
+    const user = get(loginUserState);
+    if (user) {
+      return user.photoURL;
+    }
+  },
 });
