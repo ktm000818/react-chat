86f518922b3e458fcddb5e9e26b920d007aebb72
ktm000818 <kimtm0818@gmail.com>
2023-06-14 22:31:47 +0900

    Feat: 채팅방 생성 및 불러오기 기능 개발

---
+++ src/firebaseRTDB-actions/chatroom/actions.ts
@@ -0,0 +1,39 @@
+import { database } from "@/firebaseModule";
+import { push, ref, set, child, get } from "firebase/database";
+
+export interface Props {
+  roomName: string;
+  description: string;
+}
+
+const TABLE = "chatroom";
+
+export const getAllChatRoomList = async () => {
+  try {
+    const dbRef = ref(database);
+    const result = await get(child(dbRef, `${TABLE}`));
+    const resultVal = await result.val();
+
+    if(result.exists()){
+      return Object.values(resultVal);
+    }else{
+      return [];
+    }
+  } catch (error) {
+    return [];
+  }
+
+}
+export const addChatRoom = async ({ roomName, description }: Props) => {
+  const currentTime = new Date().toUTCString();
+  const postListRef = ref(database, `${TABLE}/${roomName}`);
+  await set(postListRef, {
+      roomName,
+      description,
+      createdAt: currentTime,
+      updatedAt: null,
+      lastMessage: null,
+      members: [],
+      roomId: window.crypto.randomUUID(),
+  });
+};
