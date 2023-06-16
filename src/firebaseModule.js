a3f2dea0e22181cd72944b137713a9d596d4e562
ktm000818 <kimtm0818@gmail.com>
2023-06-16 23:14:40 +0900

    [1]Fix: api 키 변경 및 숨김

--- src/firebaseModule.js
+++ src/firebaseModule.js
@@ -13,15 +13,14 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdCO63_XcsjNl-xTKDWGCNw_vw3aGrHCQ",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "next-firebase-chat-b2121.firebaseapp.com",
   projectId: "next-firebase-chat-b2121",
   storageBucket: "next-firebase-chat-b2121.appspot.com",
   messagingSenderId: "966375309261",
   appId: "1:966375309261:web:7a1fdb48d15e2c3f850e84",
   measurementId: "G-GX0V05DHR0",
-  databaseURL:
-    "https://next-firebase-chat-b2121-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  databaseURL: "https://next-firebase-chat-b2121-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
