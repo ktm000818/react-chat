c1dd10d70873f20ff5c5923231952eb179870d37
ktm000818 <kimtm0818@gmail.com>
2023-06-20 22:49:04 +0900

    Feat: 리코일 스토리지 .ts로 변경

--- src/recoil/recoil-store/store.ts
+++ src/recoil/recoil-store/store.ts
@@ -2,19 +2,30 @@ import { atom, selector } from "recoil";
 import { recoilPersist } from "recoil-persist";
 const { persistAtom } = recoilPersist();
 
-export const sessionState = atom({
+export const sessionState = atom<any>({
   key: "sessionState",
   default: null,
   effects: [persistAtom],
 });
 
-export const chatRoomIdState = atom({
+export const chatRoomIdState = atom<string>({
   key: "chatRoomIdState",
-  default: null,
+  default: '',
   effects: [persistAtom],
 });
 
-export const isLoggedInSelector = selector({
+interface ChatRoomInfo {
+roomId : string,
+roomName : string
+}
+
+export const chatRoomInfoState = atom<ChatRoomInfo>({
+  key: "chatRoomInfoState",
+  default: {roomId: "", roomName: ""},
+  effects: [persistAtom],
+});
+
+export const isLoggedInSelector = selector<boolean>({
   key: "isLoggedInSelector",
   get: ({ get }) => {
     const user = get(sessionState);
@@ -26,22 +37,26 @@ export const isLoggedInSelector = selector({
   },
 });
 
-export const userNameSelector = selector({
+export const userNameSelector = selector<string | null>({
   key: "userName",
   get: ({ get }) => {
     const user = get(sessionState);
     if (user) {
       return user.displayName;
+    }else{
+      return null;
     }
   },
 });
 
-export const userImageSelector = selector({
+export const userImageSelector = selector<string>({
   key: "userImage",
   get: ({ get }) => {
     const user = get(sessionState);
     if (user) {
       return user.photoURL;
+    }else{
+      return '';
     }
   },
 });
