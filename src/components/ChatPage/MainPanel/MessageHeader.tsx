192d865b790bbbba875d8336adf8dacf7868b883
ktm000818 <kimtm0818@gmail.com>
2023-06-28 00:49:15 +0900

    Feat: 즐겨찾기 버튼 메시지헤더로 이동

--- src/components/ChatPage/MainPanel/MessageHeader.tsx
+++ src/components/ChatPage/MainPanel/MessageHeader.tsx
@@ -1,9 +1,40 @@
 import { useRecoilValue } from "recoil";
 import UserPanel from "./UserPanel";
-import { chatRoomInfoState } from "@/recoil/recoil-store/store";
+import { chatRoomInfoState, sessionState } from "@/recoil/recoil-store/store";
+import { database } from "@/firebaseModule";
+import { onChildAdded, onChildRemoved, ref } from "firebase/database";
+import { getAllChatRoomListByUID } from "@/firebase-actions/chatroom/actions";
+import {
+  addFavorite,
+  getIsFavoriteByRoomId,
+  removeFavorite,
+} from "@/firebase-actions/chatroom/favorites/actions";
+import { useEffect, useState } from "react";
 
 function MessagesHeader() {
+  const user = useRecoilValue(sessionState);
   const chatRoomInfo = useRecoilValue(chatRoomInfoState);
+
+  const [isFavorite, setIsFavorite] = useState<any>(null);
+
+  useEffect(() => {
+    async function getIsFavorite() {
+      const isFavorite = await getIsFavoriteByRoomId(
+        user.uid,
+        chatRoomInfo.roomId
+      );
+      setIsFavorite(isFavorite);
+    }
+
+    const userFavoritesRef = ref(database, `user_favorites/${user.uid}`);
+
+    onChildAdded(userFavoritesRef, () => {
+      getIsFavorite();
+    });
+    onChildRemoved(userFavoritesRef, () => {
+      getIsFavorite();
+    });
+  }, [chatRoomInfo.roomId]);
   return (
     <div
       style={{
@@ -15,7 +46,20 @@ function MessagesHeader() {
         height: "100%",
       }}
     >
-      <div> {chatRoomInfo.roomName} </div>
+      <div>
+        <h5>{chatRoomInfo.roomName}</h5>
+        <img
+          src={isFavorite ? "filled_star.svg" : "star.svg"}
+          alt="favorite"
+          onClick={() => {
+            if (isFavorite) {
+              removeFavorite(user.uid, chatRoomInfo.roomId);
+            } else {
+              addFavorite(user.uid, chatRoomInfo);
+            }
+          }}
+        />
+      </div>
       <UserPanel />
     </div>
   );
