23c7b84d0a34233da9c443a2add8915283903b8b
ktm000818 <kimtm0818@gmail.com>
2023-06-28 00:50:05 +0900

    Feat: Favorite actions 추가 및 수정

--- src/firebase-actions/chatroom/favorites/actions.ts
+++ src/firebase-actions/chatroom/favorites/actions.ts
@@ -1,30 +1,84 @@
 import { database } from "@/firebaseModule";
 import { child, get, push, ref, remove, set } from "firebase/database";
 
-const TABLE = "favorites";
+const FAVORITES = "favorites";
+const USER_FAVORITES = "user_favorites";
 
-type FavoriteAddProps = (uid: string, roomId: string) => Promise<void>;
-type FavoriteRemoveProps = (roomId: string) => Promise<void>;
+type FavoriteAddProps = (
+  uid: string,
+  room: { roomId: string }
+) => Promise<void>;
+type FavoriteRemoveProps = (uid: string, roomId: string) => Promise<void>;
 
-export const addFavorite: FavoriteAddProps = async (uid, roomId) => {
-  const favoritesRef = ref(database, `${TABLE}`);
+export const addFavorite: FavoriteAddProps = async (uid, room) => {
+  const favoritesRef = ref(database, `${FAVORITES}`);
+  const userFavoritesRef = ref(
+    database,
+    `${USER_FAVORITES}/${uid}/${room.roomId}`
+  );
   const newFavoriteRef = push(favoritesRef);
-  await set(newFavoriteRef, {
-    uid,
-    roomId
-  });
+
+  const data = { ...room, uid };
+
+  await set(newFavoriteRef, data);
+  await set(userFavoritesRef, data);
 };
 
-export const removeFavorite: FavoriteRemoveProps = async (roomId) => {
-  const docKey = await getFavoriteDocKeyByRoomId(roomId);
+export const removeFavorite: FavoriteRemoveProps = async (uid, roomId) => {
+  const getFavoriteDocKeyByRoomId: (
+    roomId: string
+  ) => Promise<string[] | null> = async (roomId) => {
+    try {
+      const dbRef = ref(database);
 
-  if(!docKey){
+      const favorites = await get(child(dbRef, `${FAVORITES}`));
+      const userFavoriteRooms = await get(
+        child(dbRef, `${USER_FAVORITES}/${uid}`)
+      );
+
+      const favoritesVal: Favorites = await favorites.val();
+      const userFavoritesVal: Favorites = await userFavoriteRooms.val();
+
+      let result: string[] = [];
+
+      if (favorites.exists() && userFavoriteRooms.exists()) {
+        for (let favoriteDocKey in favoritesVal) {
+          if (favoritesVal[favoriteDocKey].roomId === roomId) {
+            result.push(favoriteDocKey);
+            break;
+          }
+        }
+
+        for (let userFavoriteRoomKey in userFavoritesVal) {
+          if (userFavoriteRoomKey === roomId) {
+            result.push(userFavoriteRoomKey);
+            break;
+          }
+        }
+        return result;
+      } else {
+        return null;
+      }
+    } catch (error) {
+      return null;
+    }
+  };
+
+  const docKeys = await getFavoriteDocKeyByRoomId(roomId);
+
+  if (!docKeys) {
     return;
   }
 
-  const favoriteRef = ref(database, `${TABLE}/${docKey}`);
-  await remove(favoriteRef);
-}
+  const favoriteDocRef = ref(database, `${FAVORITES}/${docKeys[0]}`);
+  const userFavoriteDocRef = ref(
+    database,
+    `${USER_FAVORITES}/${uid}/${docKeys[1]}`
+  );
+
+  await remove(favoriteDocRef);
+  await remove(userFavoriteDocRef);
+};
 
 export interface Favorite {
   uid: string;
@@ -35,21 +89,16 @@ interface Favorites {
   [key: string]: Favorite;
 }
 
-export const getFavoriteDocKeyByRoomId: (roomId: string) => Promise<string | null> = async (roomId) => {
+export const getFavoritesByUID: (
+  uid: string
+) => Promise<Favorite[] | null> = async (uid) => {
   try {
     const dbRef = ref(database);
-    const favorites = await get(child(dbRef, `${TABLE}`));
-    const favoritesVal:Favorites = await favorites.val();
-    let result = '';
+    const result = await get(child(dbRef, `${USER_FAVORITES}/${uid}`));
+    const resultVal: Favorites = await result.val();
 
-    if (favorites.exists()) {
-      for(const favorite in favoritesVal){
-        if(favoritesVal[favorite].roomId === roomId){
-          result = favorite;
-          break;
-        }
-      }
-      return result;
+    if (result.exists()) {
+      return Object.values(resultVal);
     } else {
       return null;
     }
@@ -58,19 +107,29 @@ export const getFavoriteDocKeyByRoomId: (roomId: string) => Promise<string | nul
   }
 };
 
-
-export const getFavoritesByUID: (uid: string) => Promise<Favorite[] | null> = async (uid) => {
+export const getIsFavoriteByRoomId: (
+  uid: string,
+  roomId: string
+) => Promise<boolean> = async (uid, roomId) => {
   try {
     const dbRef = ref(database);
-    const result = await get(child(dbRef, `${TABLE}`));
-    const resultVal:Favorites  = await result.val();
+    const result = await get(child(dbRef, `${USER_FAVORITES}/${uid}`));
+
+    const resultVal = await result.val();
+
+    console.log(resultVal, roomId);
 
     if (result.exists()) {
-      return Object.values(resultVal).filter((favoriteRoom) => favoriteRoom.uid === uid);
+      for (let docKeyRoomId in resultVal) {
+        if (docKeyRoomId === roomId) {
+          return true;
+        }
+      }
+      return false;
     } else {
-      return null;
+      return false;
     }
   } catch (error) {
-    return null;
+    return false;
   }
 };
