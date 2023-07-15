8b532a76b545c96dedea2a36db4512b6782b4521
ktm000818 <kimtm0818@gmail.com>
2023-07-15 22:41:39 +0900

    Feat: 채팅방 즐겨찾기 유무를 변경하는 기능 적용

--- src/firebase-actions/chatroom/favorites/actions.ts
+++ src/firebase-actions/chatroom/favorites/actions.ts
@@ -1,40 +1,41 @@
 import { database } from "@/firebaseModule";
-import { child, get, push, ref, remove, set } from "firebase/database";
+import { child, get, push, ref, remove, set, update } from "firebase/database";
 
 const FAVORITES = "favorites";
 const USER_FAVORITES = "user_favorites";
+const USER_CHATROOM = "user_chatroom";
 
-type FavoriteAddProps = (
-  uid: string,
-  room: { roomId: string }
-) => Promise<void>;
+type FavoriteAddProps = (uid: string, room: { roomId: string }) => Promise<void>;
 type FavoriteRemoveProps = (uid: string, roomId: string) => Promise<void>;
+type ChangingIsFavoriteFuncProps = (uid: string, roomId: string, flag: boolean) => Promise<void>;
+
+const changeIsFavoritePropertieOfChatRoom: ChangingIsFavoriteFuncProps = async (uid, roomId, flag) => {
+  const dbRef = ref(database);
+
+  const updates: any = {};
+  updates[`${USER_CHATROOM}/${uid}/${roomId}/isFavorite`] = flag;
+  await update(dbRef, updates);
+};
 
 export const addFavorite: FavoriteAddProps = async (uid, room) => {
   const favoritesRef = ref(database, `${FAVORITES}`);
-  const userFavoritesRef = ref(
-    database,
-    `${USER_FAVORITES}/${uid}/${room.roomId}`
-  );
+  const userFavoritesRef = ref(database, `${USER_FAVORITES}/${uid}/${room.roomId}`);
   const newFavoriteRef = push(favoritesRef);
 
   const data = { ...room, uid };
 
   await set(newFavoriteRef, data);
   await set(userFavoritesRef, data);
+  await changeIsFavoritePropertieOfChatRoom(uid, room.roomId, true);
 };
 
 export const removeFavorite: FavoriteRemoveProps = async (uid, roomId) => {
-  const getFavoriteDocKeyByRoomId: (
-    roomId: string
-  ) => Promise<string[] | null> = async (roomId) => {
+  const getFavoriteDocKeyByRoomId: (roomId: string) => Promise<string[] | null> = async (roomId) => {
     try {
       const dbRef = ref(database);
 
       const favorites = await get(child(dbRef, `${FAVORITES}`));
-      const userFavoriteRooms = await get(
-        child(dbRef, `${USER_FAVORITES}/${uid}`)
-      );
+      const userFavoriteRooms = await get(child(dbRef, `${USER_FAVORITES}/${uid}`));
 
       const favoritesVal: Favorites = await favorites.val();
       const userFavoritesVal: Favorites = await userFavoriteRooms.val();
@@ -71,13 +72,11 @@ export const removeFavorite: FavoriteRemoveProps = async (uid, roomId) => {
   }
 
   const favoriteDocRef = ref(database, `${FAVORITES}/${docKeys[0]}`);
-  const userFavoriteDocRef = ref(
-    database,
-    `${USER_FAVORITES}/${uid}/${docKeys[1]}`
-  );
+  const userFavoriteDocRef = ref(database, `${USER_FAVORITES}/${uid}/${docKeys[1]}`);
 
   await remove(favoriteDocRef);
   await remove(userFavoriteDocRef);
+  await changeIsFavoritePropertieOfChatRoom(uid, roomId, false);
 };
 
 export interface Favorite {
@@ -89,9 +88,7 @@ interface Favorites {
   [key: string]: Favorite;
 }
 
-export const getFavoritesByUID: (
-  uid: string
-) => Promise<Favorite[] | null> = async (uid) => {
+export const getFavoritesByUID: (uid: string) => Promise<Favorite[]> = async (uid) => {
   try {
     const dbRef = ref(database);
     const result = await get(child(dbRef, `${USER_FAVORITES}/${uid}`));
@@ -100,24 +97,19 @@ export const getFavoritesByUID: (
     if (result.exists()) {
       return Object.values(resultVal);
     } else {
-      return null;
+      return [];
     }
   } catch (error) {
-    return null;
+    return [];
   }
 };
 
-export const getIsFavoriteByRoomId: (
-  uid: string,
-  roomId: string
-) => Promise<boolean> = async (uid, roomId) => {
+export const getIsFavoriteByRoomId: (uid: string, roomId: string) => Promise<boolean> = async (uid, roomId) => {
   try {
     const dbRef = ref(database);
     const result = await get(child(dbRef, `${USER_FAVORITES}/${uid}`));
 
     const resultVal = await result.val();
-
-    console.log(resultVal, roomId);
 
     if (result.exists()) {
       for (let docKeyRoomId in resultVal) {
