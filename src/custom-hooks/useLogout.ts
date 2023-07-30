b96a983cc0c86b4b3a946581572c7e72881b217a
ktm000818 <kimtm0818@gmail.com>
2023-07-30 23:28:28 +0900

    Refactor: useLogout 리팩토링

--- src/custom-hooks/useLogout.ts
+++ src/custom-hooks/useLogout.ts
@@ -1,5 +1,4 @@
-import { useRecoilState, useSetRecoilState } from "recoil";
-import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import { signOut } from "firebase/auth";
 import { auth, database } from "@/firebaseModule";
 import { sessionState } from "@/recoil/recoil-store/store";
@@ -7,15 +6,24 @@ import { ref, update } from "@firebase/database";
 
 export function useLogout() {
   const [session, setSession] = useRecoilState(sessionState);
+
+  const updateLoginStateToFalse = async () => {
+    if (!session) {
+      return;
+    }
+
+    const dbRef = ref(database);
+    const updates: any = {};
+    updates[`users/${session.uid}/isLogin`] = false;
+    await update(dbRef, updates);
+  };
+
   const logout = async () => {
-    signOut(auth)
+    await signOut(auth)
       .then(async () => {
-        const dbRef = ref(database);
-        const updates: any = {};
-        updates[`users/${session.uid}/isLogin`] = false;
-        await update(dbRef, updates);
-        setSession(null);
         window.location.href = "/";
+        updateLoginStateToFalse();
+        setSession(null);
       })
       .catch((error) => {
         alert("로그아웃 할 수 없습니다. 잠시 후 시도해주세요.");
