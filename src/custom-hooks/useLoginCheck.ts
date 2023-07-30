cd8ebfef4180b96661645d81604e54379f5a9705
ktm000818 <kimtm0818@gmail.com>
2023-07-30 23:28:57 +0900

    Refactor: useLoginCheck hook 리팩토링

--- src/custom-hooks/useLoginCheck.ts
+++ src/custom-hooks/useLoginCheck.ts
@@ -1,26 +1,24 @@
 import { sessionState } from "@/recoil/recoil-store/store";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
 export default function useLoginCheck() {
-  const user = useRecoilValue(sessionState);
   const navigate = useNavigate();
-  const ACCESS_PAGES = ["/chat", "/"];
-  const ACCESS_FOR_UNKNOWN_PAGES = ["/login", "/register", "/"];
+  const user = useRecoilValue(sessionState);
+  const ACCESS_PAGES = useMemo(() => ["/chat", "/"], []);
+  const ACCESS_PAGES_FOR_UNKNOWN = useMemo(() => ["/login", "/register", "/"], []);
   const currLocation = window.location.pathname;
 
   useEffect(() => {
     if (user) {
-      //로그인 완료됐을 때
       if (!ACCESS_PAGES.includes(currLocation)) {
         navigate("/");
       }
     } else {
-      //로그인 되지않았을 때
-      if (!ACCESS_FOR_UNKNOWN_PAGES.includes(currLocation)) {
+      if (!ACCESS_PAGES_FOR_UNKNOWN.includes(currLocation)) {
         navigate("/");
       }
     }
-  }, []);
+  }, [ACCESS_PAGES, ACCESS_PAGES_FOR_UNKNOWN, currLocation, navigate, user]);
 }
