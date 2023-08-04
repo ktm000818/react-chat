05505169644c043e7c0a7359398972af439c5339
ktm000818 <kimtm0818@gmail.com>
2023-08-04 22:40:08 +0900

    Refactor: ChatRooms 리팩토링

--- src/components/ChatPage/SidePanel/ChatRooms.tsx
+++ src/components/ChatPage/SidePanel/ChatRooms.tsx
@@ -1,39 +1,29 @@
 import ChatRoomModalOpenButton from "@/common/components/Modals/ChatRoomModalOpenButton";
-import { getAllChatRoomListByUID } from "@/firebase-actions/chatroom/actions";
+import { ChatRoom, getAllChatRoomListByUID } from "@/firebase-actions/chatroom/actions";
 import { database } from "@/firebaseModule";
-import { chatRoomIdState, chatRoomInfoState, chatRoomListState, sessionState } from "@/recoil/recoil-store/store";
+import { chatRoomIdState, chatRoomInfoState, sessionState } from "@/recoil/recoil-store/store";
 import { onChildAdded, onChildChanged, onChildRemoved, ref } from "@firebase/database";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function ChatRooms() {
-  const [chatRooms, setChatRooms] = useRecoilState(chatRoomListState);
   const user = useRecoilValue(sessionState);
   const [chatRoomId, setChatRoomId] = useRecoilState(chatRoomIdState);
   const setChatRoomInfo = useSetRecoilState(chatRoomInfoState);
+  const [chatRooms, setChatRooms] = useState<ChatRoom[]>([]);
 
   const getAndSetChatRooms = useCallback(async () => {
-    if (!user) {
-      return;
-    }
+    if (!user) return;
     const rooms = await getAllChatRoomListByUID(user.uid);
     setChatRooms(rooms);
-  }, [user, setChatRooms]);
+  }, [user]);
 
   useEffect(() => {
-    if (!user) {
-      return;
-    }
+    if (!user) return;
     const chatRoomsRef = ref(database, `user_chatroom/${user.uid}`);
-    onChildAdded(chatRoomsRef, () => {
-      getAndSetChatRooms();
-    });
-    onChildRemoved(chatRoomsRef, () => {
-      getAndSetChatRooms();
-    });
-    onChildChanged(chatRoomsRef, () => {
-      getAndSetChatRooms();
-    });
+    onChildAdded(chatRoomsRef, getAndSetChatRooms);
+    onChildRemoved(chatRoomsRef, getAndSetChatRooms);
+    onChildChanged(chatRoomsRef, getAndSetChatRooms);
   }, [getAndSetChatRooms, user]);
 
   return (
@@ -51,26 +41,25 @@ export default function ChatRooms() {
           }}
         ></div>
         <div style={{ display: "flex", flexDirection: "column" }}>
-          {chatRooms &&
-            chatRooms.map((room: any, index: any) => {
-              return (
-                <div
-                  key={index + "rooms"}
-                  style={{
-                    textDecoration: chatRoomId === room.roomId ? "underline" : "none",
-                  }}
-                  onClick={() => {
-                    setChatRoomId(room.roomId);
-                    setChatRoomInfo({
-                      roomId: room.roomId,
-                      roomName: room.roomName,
-                    });
-                  }}
-                >
-                  <span style={{ cursor: "pointer" }}>{room.roomName}</span>
-                </div>
-              );
-            })}
+          {(chatRooms || []).map((room: ChatRoom, _) => {
+            return (
+              <div
+                key={room.roomId}
+                style={{
+                  textDecoration: chatRoomId === room.roomId ? "underline" : "none",
+                }}
+                onClick={() => {
+                  setChatRoomId(room.roomId);
+                  setChatRoomInfo({
+                    roomId: room.roomId,
+                    roomName: room.roomName,
+                  });
+                }}
+              >
+                <span style={{ cursor: "pointer" }}>{room.roomName}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
