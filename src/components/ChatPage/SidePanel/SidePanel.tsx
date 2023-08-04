a72e2249fcecae1ccc5cb3a4a5bc8f245df7866c
ktm000818 <kimtm0818@gmail.com>
2023-08-04 23:00:42 +0900

    Style: SidePanel scss 적용

--- src/components/ChatPage/SidePanel/SidePanel.tsx
+++ src/components/ChatPage/SidePanel/SidePanel.tsx
@@ -1,26 +1,15 @@
 import ChatRooms from "./ChatRooms";
 import Favorited from "./Favorited";
+import styles from "@styles/Chat/SidePanel/SidePanel.module.scss";
+import Logo from "./Logo";
 
 function SidePanel() {
   return (
-    <>
-      <div
-        style={{
-          backgroundColor: "#2c2c2c",
-          padding: "2rem",
-          minHeight: "100vh",
-          minWidth: "250px",
-          color: "white",
-        }}
-      >
-        <div style={{ padding: "10px", backgroundColor: "#66ff00", fontSize: "2rem", textAlign: "center", marginBottom: "30px" }}>
-          <span style={{ textShadow: "1px 1px black", fontWeight: "bold" }}>RELACKS</span>
-        </div>
-        {/* <UserPanel /> */}
-        <Favorited />
-        <ChatRooms />
-      </div>
-    </>
+    <div className={styles["container"]}>
+      <Logo />
+      <Favorited />
+      <ChatRooms />
+    </div>
   );
 }
 
