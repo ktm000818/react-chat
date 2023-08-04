11a148ce0129a989e110bf1db3aa2de421b9e6b9
ktm000818 <kimtm0818@gmail.com>
2023-08-04 22:40:55 +0900

    Refactor: Favorited 리팩토링

--- src/components/ChatPage/SidePanel/Favorited.tsx
+++ src/components/ChatPage/SidePanel/Favorited.tsx
@@ -1,61 +1,51 @@
-import { getFavoritesByUID } from "@/firebase-actions/chatroom/favorites/actions";
+import { Favorite, getFavoritesByUID } from "@/firebase-actions/chatroom/favorites/actions";
 import { database } from "@/firebaseModule";
-import { chatRoomIdState, chatRoomInfoState, favoritesListState, sessionState } from "@/recoil/recoil-store/store";
+import { chatRoomIdState, chatRoomInfoState, sessionState } from "@/recoil/recoil-store/store";
 import { onChildAdded, onChildRemoved, ref } from "firebase/database";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, Fragment, useState } from "react";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function Favorited() {
   const user = useRecoilValue(sessionState);
-  const [favorites, setFavorites] = useRecoilState(favoritesListState);
+  const [favorites, setFavorites] = useState<Favorite[]>([]);
   const [chatRoomId, setChatRoomId] = useRecoilState(chatRoomIdState);
   const setChatRoomInfo = useSetRecoilState(chatRoomInfoState);
 
   const getAndSetFavorites = useCallback(async () => {
-    if (!user) {
-      return;
-    }
-    const rooms = await getFavoritesByUID(user.uid);
+    if (!user) return;
+    const rooms: Favorite[] = await getFavoritesByUID(user.uid);
     setFavorites(rooms);
-  }, [setFavorites, user]);
+  }, [user]);
 
   useEffect(() => {
-    if (!user) {
-      return;
-    }
+    if (!user) return;
     const userFavoritesRef = ref(database, `user_favorites/${user.uid}`);
-    onChildAdded(userFavoritesRef, () => {
-      getAndSetFavorites();
-    });
-    onChildRemoved(userFavoritesRef, () => {
-      getAndSetFavorites();
-    });
+    onChildAdded(userFavoritesRef, getAndSetFavorites);
+    onChildRemoved(userFavoritesRef, getAndSetFavorites);
   }, [getAndSetFavorites, user]);
 
   return (
     <>
       Favorites
       <div style={{ margin: 10 }}>
-        {favorites &&
-          favorites.map((room: any, index: number) => (
-            <>
-              <div
-                key={index + "rooms"}
-                style={{
-                  textDecoration: chatRoomId === room.roomId ? "underline" : "none",
-                }}
-                onClick={() => {
-                  setChatRoomId(room.roomId);
-                  setChatRoomInfo({
-                    roomId: room.roomId,
-                    roomName: room.roomName,
-                  });
-                }}
-              >
-                <span style={{ cursor: "pointer" }}>{room.roomName}</span>
-              </div>
-            </>
-          ))}
+        {(favorites || []).map((room: Favorite, _) => (
+          <Fragment key={room.roomId}>
+            <div
+              style={{
+                textDecoration: chatRoomId === room.roomId ? "underline" : "none",
+              }}
+              onClick={() => {
+                setChatRoomId(room.roomId);
+                setChatRoomInfo({
+                  roomId: room.roomId,
+                  roomName: room.roomName,
+                });
+              }}
+            >
+              <span style={{ cursor: "pointer" }}>{room.roomName}</span>
+            </div>
+          </Fragment>
+        ))}
       </div>
     </>
   );
