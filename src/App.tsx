95491dbd0b3e27f2b143451ddb7f9e5695d53823
ktm000818 <kimtm0818@gmail.com>
2023-08-29 01:29:42 +0900

    Fix: 로그인 유지 기능 버그 픽스

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import LoginPage from "@components/LoginPage/LoginPage";
 import RegisterPage from "@components/RegisterPage/RegisterPage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ChatPage from "./components/ChatPage/ChatPage";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebaseModule";
+import { useSetRecoilState } from "recoil";
+import { userAuthState } from "./recoil/recoil-store/store";
 
 export default function App() {
+  const setUserAuthState = useSetRecoilState(userAuthState);
+
+  useEffect(() => {
+    const unscribe = onAuthStateChanged(auth, (user) => {
+      setUserAuthState(user);
+    });
+
+    return () => unscribe();
+  }, [setUserAuthState]);
+
   const router = createBrowserRouter([
     {
       path: "/",
