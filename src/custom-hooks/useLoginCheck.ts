68198010693580398fb0e672c034ad4f68e1e7a7
ktm000818 <kimtm0818@gmail.com>
2024-01-25 01:00:53 +0900

    Refactor: 엑세스페이지 경로 정의변수 전역으로 이동

--- src/custom-hooks/useLoginCheck.ts
+++ src/custom-hooks/useLoginCheck.ts
@@ -4,11 +4,12 @@ import { useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
+const ACCESS_PAGES = ["/chat", "/"];
+const ACCESS_PAGES_FOR_UNKNOWN = ["/login", "/register", "/"];
+
 export default function useLoginCheck() {
   const navigate = useNavigate();
   const user = useRecoilValue(userAuthState);
-  const ACCESS_PAGES = useMemo(() => ["/chat", "/"], []);
-  const ACCESS_PAGES_FOR_UNKNOWN = useMemo(() => ["/login", "/register", "/"], []);
   const currLocation = window.location.pathname;
 
   useEffect(() => {
