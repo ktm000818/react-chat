2135455c19fd909bdbc55932d165a7254550f10f
ktm000818 <kimtm0818@gmail.com>
2024-02-28 23:47:53 +0900

    Style: Apply MainPage responsible layout

--- src/components/HomePage/MainLogo.tsx
+++ src/components/HomePage/MainLogo.tsx
@@ -1,5 +1,5 @@
 import styles from "@styles/MainLogo.module.scss";
 
 export default function MainLogo() {
-  return <img className={styles["noDrag"]} src={"logo/logo_transparent.png"} width={100} height={100} alt="logo" />;
+  return <img className={styles["image"]} src={"logo/logo_transparent.png"} width={100} height={100} alt="logo" />;
 }
